89a1939f7ae8b7c3c45f4012ffc435bc3bc66840
ceilingfan6445 <140544489+ceilingfan6445@users.noreply.github.com>
2025-07-01 11:41:05 -0700

    Commented out code to allow @Component use

--- 4_dependency_injection/0_no_dependency_injection/colours/src/main/java/com/deviro/maven/services/config/PrinterConfig.java
+++ 4_dependency_injection/0_no_dependency_injection/colours/src/main/java/com/deviro/maven/services/config/PrinterConfig.java
@@ -2,41 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+// Need to import classes again to reactivate this config file (use alt+enter)
 package com.deviro.maven.services.config;
 
-import com.deviro.maven.services.BluePrinter;
-import com.deviro.maven.services.ColourPrinter;
-import com.deviro.maven.services.GreenPrinter;
-import com.deviro.maven.services.RedPrinter;
-import com.deviro.maven.services.impl.ColourPrinterImpl;
-import com.deviro.maven.services.impl.SpanishBluePrinter;
-import com.deviro.maven.services.impl.SpanishGreenPrinter;
-import com.deviro.maven.services.impl.SpanishRedPrinter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 /**
- * @author hackathon
+ * @author hackathon @Configuration public class PrinterConfig { @Bean public BluePrinter
+ *     bluePrinter() { return new SpanishBluePrinter(); } @Bean public GreenPrinter greenPrinter() {
+ *     return new SpanishGreenPrinter(); } @Bean public RedPrinter redPrinter() { return new
+ *     SpanishRedPrinter(); } @Bean public ColourPrinter colourPrinter(RedPrinter red, BluePrinter
+ *     blue, GreenPrinter green) { return new ColourPrinterImpl(red, blue, green); } }
  */
-@Configuration
-public class PrinterConfig {
-  @Bean
-  public BluePrinter bluePrinter() {
-    return new SpanishBluePrinter();
-  }
-
-  @Bean
-  public GreenPrinter greenPrinter() {
-    return new SpanishGreenPrinter();
-  }
-
-  @Bean
-  public RedPrinter redPrinter() {
-    return new SpanishRedPrinter();
-  }
-
-  @Bean
-  public ColourPrinter colourPrinter(RedPrinter red, BluePrinter blue, GreenPrinter green) {
-    return new ColourPrinterImpl(red, blue, green);
-  }
-}
