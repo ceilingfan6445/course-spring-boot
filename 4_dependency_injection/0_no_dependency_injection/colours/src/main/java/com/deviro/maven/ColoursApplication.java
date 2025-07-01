0a2ce496299d8f682cfa242bf554d9b64d519bd6
ceilingfan6445 <140544489+ceilingfan6445@users.noreply.github.com>
2025-06-30 22:44:44 -0700

    Defined start-class and imported different libraries to enable logger

--- 4_dependency_injection/0_no_dependency_injection/colours/src/main/java/com/deviro/maven/ColoursApplication.java
+++ 4_dependency_injection/0_no_dependency_injection/colours/src/main/java/com/deviro/maven/ColoursApplication.java
@@ -3,6 +3,8 @@ package com.deviro.maven;
 import com.deviro.maven.services.ColourPrinter;
 import com.deviro.maven.services.impl.ColourPrinterImpl;
 import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Log
 public class ColoursApplication implements CommandLineRunner {
+
+  private static final Logger log = LoggerFactory.getLogger(ColoursApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(ColoursApplication.class, args);
